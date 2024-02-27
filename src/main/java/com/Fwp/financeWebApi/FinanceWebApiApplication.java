74bda451cd1895ce24a3d4e2d166cf87d58f56f9
rafael <rafaelmarinhoalves5@gmail.com>
2024-02-26 22:07:17 -0300

    intial commit

---
+++ src/main/java/com/Fwp/financeWebApi/FinanceWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.Fwp.financeWebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fwp/financeWebApi/FinanceWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fwp.financeWebApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
